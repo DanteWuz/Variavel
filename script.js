09df7c2201e3ee4fa8156fc7165c59abb41123d3
Diogo <164107271+DanteWuz@users.noreply.github.com>
2024-05-13 13:03:46 -0300

    V1 código variável
    Primeira versão do código

---
+++ script.js
@@ -0,0 +1,12 @@
+function amazenarValor(){
+
+var userInput = document.getElementById("userInput").value;
+console.log ("O valor da variavel e:" + userInput)
+document.getElementById("valorInserido").innerText = "O valor inserido e :" + userInput;
+
+
+
+
+
+
+}
